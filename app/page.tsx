e80b4bdb790998071022041280b4e48fbaf7f76b
jpal91 <pallanschja@gmail.com>
2024-11-26 14:51:18 -0500

    Made form submit a server action instead

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 // Simple sign-up form example with input validation
+import { redirect } from "next/navigation";
+
+const submit = async (formData: FormData) => {
+  "use server";
+
+  const name = formData.get("email");
+
+  if (name === "fail@gmail.com") {
+    throw new Error();
+  }
+
+  redirect("/thank-you");
+};
 
 export default function Page() {
   return (
     <div className="flex flex-col prose lg:prose-lg bg-base-100 h-fit border border-neutral rounded-lg shadow-lg p-6">
       <h2>Sign Up For Our Awesome Service! 🎉</h2>
-      <form
-        action="/api/sign-up"
-        method="POST"
-        className="group flex flex-col gap-4"
-      >
+      <form action={submit} className="group flex flex-col gap-4">
         <label className="input input-bordered flex items-center gap-2 valid-input">
           <span className="font-bold">First Name</span>
           <input name="firstName" type="text" className="grow" required />
